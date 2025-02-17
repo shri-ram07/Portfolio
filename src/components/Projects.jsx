baa07e89bd3d6882f7c5bb9db870a218f9ce8439
Shri Ram dwivedi <shahiananya381@gmail.com>
2025-02-17 14:54:07 +0530

    Final

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,29 +1,29 @@
 
 import React from "react";
-import Sym from 'C:/Users/rauna/OneDrive/Desktop/Projects/Ram_Portfolio/src/assets/Sym.jpg'
-import std from 'C:/Users/rauna/OneDrive/Desktop/Projects/Ram_Portfolio/src/assets/std.jpg'
-import power from 'C:/Users/rauna/OneDrive/Desktop/Projects/Ram_Portfolio/src/assets/power.jpg'
-import news from 'C:/Users/rauna/OneDrive/Desktop/Projects/Ram_Portfolio/src/assets/news.jpg'
-import fake from 'C:/Users/rauna/OneDrive/Desktop/Projects/Ram_Portfolio/src/assets/fake.jpg'
+import symImage from '../assets/Sym.jpg';
+import stdImage from '../assets/std.jpg';
+import powerImage from '../assets/power.jpg';
+import newsImage from '../assets/news.jpg';
+import fakeImage from '../assets/fake.jpg';
 import Footer from './Footer'
 
 const ProjectCard = ({ image, title, description, git, technologies }) => {
     return (
         <div className="max-w-sm sm:max-w-sm md:max-w-sm bg-gray-900 border border-neutral-100 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             {title=='SymptomIQ' && <a href="#">
-                <img className="w-full rounded-t-lg h-auto object-cover " src={Sym} alt="" />
+                <img className="w-full rounded-t-lg h-auto object-cover " src={symImage} alt="" />
             </a>}
             {title=='Deep Fake Detector' && <a href="#">
-                <img className="w-full rounded-t-lg h-auto object-cover " src={fake} alt="" />
+                <img className="w-full rounded-t-lg h-auto object-cover " src={newsImage} alt="" />
             </a>}
             {title=='Fake News Detection (NLP Project)' && <a href="#">
-                <img className="w-full rounded-t-lg h-auto object-cover " src={news} alt="" />
+                <img className="w-full rounded-t-lg h-auto object-cover " src={fakeImage } alt="" />
             </a>}
             {title=='Automated Power Monitoring System' && <a href="#">
-                <img className="w-full rounded-t-lg h-auto object-cover " src={power} alt="" />
+                <img className="w-full rounded-t-lg h-auto object-cover " src={powerImage} alt="" />
             </a>}
             {title=='Student Management System' && <a href="#">
-                <img className="w-full rounded-t-lg h-auto object-cover " src={std} alt="" />
+                <img className="w-full rounded-t-lg h-auto object-cover " src={stdImage} alt="" />
             </a>}
             <div className="p-4 sm:p-6">
                 <a href="#">
@@ -74,35 +74,35 @@ export const project = [
     {
         title:'SymptomIQ',
         description:'Developed a Flask-based web application that predicts health conditions using a TensorFlow model. The app integrates geolocation via Google Gemini API to provide personalized doctor recommendations, enhancing user experience and accessibility to healthcare advice.',
-        image: {Sym},
+        image: {symImage},
         git:'https://github.com/shri-ram07/SymptomIQ.git',
         technologies:['Tensorflow , Flask , HTML/CSS/JS']
     },
     {
         title:'Deep Fake Detector',
         description:'Created a web-based deepfake detection tool utilizing TensorFlow and Flask. This application allows users to upload images and classify them as "REAL" or "FAKE" using an accuracy-driven machine learning model. The interface includes interactive progress bars and animations to visually display prediction probabilities.',
-        image: {fake},
+        image: {fakeImage},
         git:'https://github.com/shri-ram07/DeepFakeDetection.git',
         technologies:['Tensorflow , Flask , HTML/CSS/JS']
     },
     {
         title:'Fake News Detection (NLP Project)',
         description:'Designed an ML-based solution employing NLP techniques to automatically detect and classify fake news articles. The project involved text pre-processing, feature extraction, and classification algorithms, achieving high accuracy in identifying misleading information.',
-        image: {news},
+        image: {newsImage},
         git:'https://github.com/shri-ram07/FakeNewsDetection.git',
         technologies:['NLTK']
     },
     {
         title:'Automated Power Monitoring System',
         description:'Developed a real-time human presence detection system using computer vision to monitor room occupancy. This project optimizes energy usage by automating appliance control based on occupancy data, showcasing innovation in energy efficiency.',
-        image: {power},
+        image: {powerImage},
         git:'https://github.com/shri-ram07/AutomatedPowerMonitoringSystem.git',
         technologies:['YOLOv3 , PyQt']
     },
     {
         title:'Student Management System',
         description:'Created a comprehensive student management system featuring a user-friendly Tkinter-based interface. The system integrates MySQL for secure and efficient data storage, implementing CRUD functionality to streamline student data handling and ensure access control.',
-        image: {std},
+        image: {stdImage},
         git:"https://github.com/shri-ram07/Student-Management-System.git",
         technologies:[ 'Python , Tkinter , MySql']
     }

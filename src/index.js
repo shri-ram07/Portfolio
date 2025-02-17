3b7f0baed0d13ebe6b5027778ba26a721e539484
Shri Ram dwivedi <shahiananya381@gmail.com>
2025-02-17 14:52:32 +0530

    Final

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
@@ -12,26 +12,17 @@ import Contact from './components/Contact';
 import Projects from './components/Projects';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Check if there's a redirect path stored in sessionStorage
-const redirectPath = sessionStorage.getItem('redirect');
-if (redirectPath) {
-  sessionStorage.removeItem('redirect');
-  window.history.replaceState(null, '', redirectPath);
-}
-
 root.render(
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/experience' element={<Experience />} />
-      <Route path='/projects' element={<Projects />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/contact' element={<Contact />} />
-    </Routes>
-    <Footer />
-  </Router>
+  <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/experience' element={<Experience/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
